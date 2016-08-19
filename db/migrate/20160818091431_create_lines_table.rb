61b7112a13aa11a0691b4c0ef2b2d413bf1aa796
Rob Dale <email": null>
2016-08-19 09:04:50 -0500

    Fix error in lines migration

--- db/migrate/20160818091431_create_lines_table.rb
+++ db/migrate/20160818091431_create_lines_table.rb
@@ -2,7 +2,7 @@ class CreateLinesTable < ActiveRecord::Migration
   def change
     create_table :lines do |t|
       t.string :name, null:false, unique:true
-      t.boolean :mode, null:false
+      t.string :mode, null:false
       t.timestamps null:false
     end
   end
